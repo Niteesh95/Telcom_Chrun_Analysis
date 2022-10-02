c9a9065dbc213ffde552ec973284acbc6e9ff61d
Niteesh95 <niteesh5595@gmail.com>
2022-10-02 12:41:06 +0100

    deployment files added

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = pickle.load(open('model_LR.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
@@ -17,11 +17,11 @@ def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    
+
     if prediction == [0]:
-        return render_template('index.html',prediction_text = 'The Customer should not churn')
-    elif prediction == [1]:       
-        return render_template('index.html',prediction_text = 'The Customer should churn')  
-    
+        return render_template('index.html',prediction_text = 'There is a Possibility that this customer will not churn')
+    elif prediction == [1]:
+        return render_template('index.html',prediction_text = 'There is a possibility that this customer will churn')
+
 if __name__ == "__main__":
     app.run(debug=True)
